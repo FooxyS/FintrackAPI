fc77b80a2bca5ba7c59e6bef82c8e70cd8ff928b
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-01 14:38:19 +0300

    Написана логика обработчика для регистрации пользователя

--- handler/register.go
+++ handler/register.go
@@ -1,7 +1,74 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type UserData struct {
+	Email string `json:"email"`
+	User
+}
+
+type UserStore interface {
+	Register(user User) error
+	Exists(username string) bool
+}
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	//проверка на правильный метод
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
+	//декодирование json запроса
+	var user UserData
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Failed to decode json request", http.StatusBadRequest)
+		return
+	}
+
+	//валидация данных пользователя
+	if user.Username == "" || user.Password == "" || user.Email == "" {
+		http.Error(w, "empty fields in json", http.StatusBadRequest)
+		return
+	}
+
+	//запись юзера в мапу
+	var users map[string]User
+
+	buf, errRead := os.ReadFile("data.json")
+	if errRead != nil {
+		http.Error(w, "failed to read the file", http.StatusInternalServerError)
+		return
+	}
+	errUnmarsh := json.Unmarshal(buf, &users)
+	if errUnmarsh != nil {
+		http.Error(w, "failed to Unmarshal the file", http.StatusInternalServerError)
+		return
+	}
+
+	if _, ok := users[user.Email]; ok {
+		http.Error(w, "User is already created", http.StatusOK)
+		return
+	}
+	users[user.Email] = user.User
+
+	//кодирование мапы
+	compbuf, errMarsh := json.Marshal(users)
+	if errMarsh != nil {
+		http.Error(w, "failed to marshal data", http.StatusInternalServerError)
+		return
+	}
+	os.WriteFile("data.json", compbuf, 0644)
+
+	http.Error(w, "user is successfully created", http.StatusCreated)
 }
