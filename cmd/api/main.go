473f2e64b46b755ce53a4746b3fe4bd64b28ea79
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-01 14:48:06 +0300

    изменение main.go, добавление логики запуска сервера

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,17 @@
-package fintrackapi
+package main
+
+import (
+	"log"
+	"net/http"
+
+	router "github.com/FooxyS/FintrackAPI/router"
+)
+
+func main() {
+	router := router.SetupRouter()
+
+	err := http.ListenAndServe("localhost:8080", router)
+	if err != nil {
+		log.Fatalf("problem with starting the server: %v\n", err)
+	}
+}
