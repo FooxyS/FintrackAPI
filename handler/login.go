63a24ca3779a939117d6970b5c75f340cf23c236
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-02 15:58:47 +0300

    написание логики обработчика /login

--- handler/login.go
+++ handler/login.go
@@ -1,1 +1,33 @@
 package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func UserLogin(w http.ResponseWriter, r *http.Request) {
+	var users []UserData
+
+	GetJson(w, &users)
+
+	var user UserData
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to decode the json data: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	for _, curUser := range users {
+		if curUser.Email == user.Email {
+			if curUser.User.Password == user.User.Password {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte("Добро пожаловать! :)"))
+				return
+			}
+			http.Error(w, "Неверный пароль", http.StatusBadRequest)
+			return
+		}
+	}
+	http.Error(w, "Пользователь не зарегестрирован", http.StatusBadRequest)
+}
