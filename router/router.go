a0393cad5b8a70aa6ed248ac00fb22bc561b09c3
Тима <TimofeyKolotovkin2@gmail.com>
2025-05-06 00:31:24 +0300

    добавление новых обработчиков

--- router/router.go
+++ router/router.go
@@ -3,12 +3,15 @@ package router
 import (
 	"net/http"
 
-	handler "github.com/FooxyS/FintrackAPI/handler"
+	handler "github.com/FooxyS/FintrackAPI/handler/auth"
 )
 
 func SetupRouter() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/register", handler.RegisterHandler)
-	mux.HandleFunc("/login", handler.UserLogin)
+	mux.HandleFunc("/auth/register", handler.RegisterHandler)
+	mux.HandleFunc("/auth/login", handler.LoginHandler)
+	mux.HandleFunc("/auth/refresh", handler.RefreshHandler)
+	mux.HandleFunc("/auth/logout", handler.LogoutHandler)
+	mux.HandleFunc("/auth/me", handler.MeHandler)
 	return mux
 }
